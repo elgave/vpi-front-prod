cbaf5d1b7f7275ec9af7b71c7af8d9647bbfefc5
Cristhian Gavegno <cgavegno21@gmail.com>
2021-11-29 22:49:56 -0300

    websocket front
    websocket front

---
+++ src/app/service/stomp.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable,of } from 'rxjs';
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StompService {
+
+  socket = new SockJS('http://localhost:8282/vapai-websocket');
+  stompClient = Stomp.over(this.socket);
+
+  subscribe(topic: string, callback:any): void {
+    const connected: boolean = this.stompClient.connected;
+    if (connected){
+      this.subscribeToTopic(topic,callback);
+      return;
+    }
+
+    this.stompClient.connect({}, (): any =>{
+      this.subscribeToTopic(topic,callback);
+    });
+
+  }
+
+  private subscribeToTopic(topic: string, callback: any):void{
+    this.stompClient.subscribe(topic, (): any =>{
+      callback();
+    })
+  }
+  
+}
